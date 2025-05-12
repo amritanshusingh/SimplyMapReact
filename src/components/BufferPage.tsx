0adf36af5af4368d89257808e86b1ca5382e684c
Amritanshu Singh <amritanshu.singh@gmail.com>
2025-05-12 19:47:02 +0530

    improved positioning of rendered map, ensured map changes style with dark theme

--- src/components/BufferPage.tsx
+++ src/components/BufferPage.tsx
@@ -1,91 +1,43 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
-import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  Pin,
-} from "@vis.gl/react-google-maps";
-
-type Poi = { key: string; location: google.maps.LatLngLiteral };
+import { Grid, Card, Box } from "@mui/material";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useTheme } from "@mui/material/styles"; // Import useTheme to access the current theme
+import { darkModeMapStyles } from "../theme/mapStyles"; // Import dark mode map styles
 
 const googleAPIkey = import.meta.env.VITE_GOOGLE_API_KEY;
 
-const locations: Poi[] = [
-  { key: "operaHouse", location: { lat: -33.8567844, lng: 151.213108 } },
-  { key: "tarongaZoo", location: { lat: -33.8472767, lng: 151.2188164 } },
-  { key: "manlyBeach", location: { lat: -33.8209738, lng: 151.2563253 } },
-  { key: "hyderPark", location: { lat: -33.8690081, lng: 151.2052393 } },
-  { key: "theRocks", location: { lat: -33.8587568, lng: 151.2058246 } },
-  { key: "circularQuay", location: { lat: -33.858761, lng: 151.2055688 } },
-  { key: "harbourBridge", location: { lat: -33.852228, lng: 151.2038374 } },
-  { key: "kingsCross", location: { lat: -33.8737375, lng: 151.222569 } },
-  { key: "botanicGardens", location: { lat: -33.864167, lng: 151.216387 } },
-  { key: "museumOfSydney", location: { lat: -33.8636005, lng: 151.2092542 } },
-  { key: "maritimeMuseum", location: { lat: -33.869395, lng: 151.198648 } },
-  { key: "kingStreetWharf", location: { lat: -33.8665445, lng: 151.1989808 } },
-  { key: "aquarium", location: { lat: -33.869627, lng: 151.202146 } },
-  { key: "darlingHarbour", location: { lat: -33.87488, lng: 151.1987113 } },
-  { key: "barangaroo", location: { lat: -33.8605523, lng: 151.1972205 } },
-];
-
-const PoiMarkers = (props: { pois: Poi[] }) => {
-  return (
-    <>
-      {props.pois.map((poi: Poi) => (
-        <AdvancedMarker key={poi.key} position={poi.location}>
-          <Pin
-            background={"#FBBC04"}
-            glyphColor={"#000"}
-            borderColor={"#000"}
-          />
-        </AdvancedMarker>
-      ))}
-    </>
-  );
-};
-
 const BufferPage: React.FC = () => {
+  const theme = useTheme(); // Access the current theme
+  const isDarkTheme = theme.palette.mode === "dark"; // Determine if the theme is dark
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100%",
-        textAlign: "center",
-        margin: "0 auto",
-        flexDirection: "column",
-      }}
-    >
-      <Typography
-        variant="h4"
-        color="text.primary"
-        sx={{ textAlign: "center", marginBottom: 2 }}
-      >
-        Buffer Landing Page
-      </Typography>
-      <APIProvider apiKey={googleAPIkey}>
-        <Map
-          mapId={import.meta.env.VITE_MAP_ID}
-          mapTypeId="terrain"
-          defaultZoom={13}
-          defaultCenter={{ lat: 25.54196730756527, lng: 77.36467900618521 }}
-          onCameraChanged={(ev) =>
-            console.log(
-              "camera changed:",
-              ev.detail.center,
-              "zoom:",
-              ev.detail.zoom
-            )
-          }
-          style={{ width: "100%", height: "80%" }}
+    <Grid sx={{ width: "100%", padding: "20px" }}>
+      <Box sx={{ height: "100vh", width: "100%" }}>
+        <Card
+          sx={{
+            height: "calc(100vh - 96px)",
+            width: "100%",
+            padding: "16px",
+            marginTop: "64px",
+          }}
         >
-          <PoiMarkers pois={locations} />
-        </Map>
-      </APIProvider>
-    </Box>
+          <div
+            style={{ position: "relative", height: "100%", width: "100%" }} // Added styles to ensure proper containment
+          >
+            <APIProvider apiKey={googleAPIkey}>
+              <Map
+                mapTypeId="terrain"
+                defaultZoom={13}
+                defaultCenter={{
+                  lat: 25.54196730756527,
+                  lng: 77.36467900618521,
+                }}
+                styles={isDarkTheme ? darkModeMapStyles : []} // Ensure dark mode styles are applied when theme is dark
+              ></Map>
+            </APIProvider>
+          </div>
+        </Card>
+      </Box>
+    </Grid>
   );
 };
 
